d9c3bd4e88887230eef1439fde44113a5b9bea46
mzj14 <mazijun2017@outlook.com>
2019-03-11 16:56:51 -0500

    now enter debug mode for example app

--- flow-scheduling-example.py
+++ flow-scheduling-example.py
@@ -3,18 +3,27 @@
 import topology
 import flows
 import scheduler
+import case_debugger
+
 
 if __name__ == '__main__':
+    '''
     nh_per_rack, nr_l, nr_s = 1, 2, 1
     n, m, port_num, router_choices = topology.leaf_spine_net(nh_per_rack, nr_l, nr_s)
-    topology.display(n, m, port_num, router_choices)
-
-    min_dest_num, max_dest_num, min_flow_num, max_flow_num, alpha, min_packet_num = 1, n - 1, 1, 3, 1.2, 10
+    min_dest_num, max_dest_num, min_flow_num, max_flow_num, alpha, min_packet_num = 1, n - 1, 1, 10, 1.2, 10
     dests, flow_num, packet_num = flows.generate(n, min_dest_num, max_dest_num, min_flow_num, max_flow_num,
                                                  alpha, min_packet_num)
 
     min_start_time, max_start_time, min_interval, max_interval = 0, 5, 2, 5
     source_timing = flows.timing(flow_num, packet_num, min_start_time, max_start_time, min_interval, max_interval)
+
+    topology.display(n, m, nh_per_rack, nr_l, nr_s, port_num, router_choices)
+    flows.display(n, dests, flow_num, packet_num, source_timing)
+    '''
+
+    flow_data_file = "backup/debug-case-1.txt"
+    n, m, nh_per_rack, nr_l, nr_s, dests, port_num, router_choices, flow_num, packet_num, source_timing = case_debugger.readFromFile(flow_data_file)
+    topology.display(n, m, nh_per_rack, nr_l, nr_s, port_num, router_choices)
     flows.display(n, dests, flow_num, packet_num, source_timing)
 
     scheduler.optimal_scheduling(n, m, dests, port_num, router_choices, flow_num, packet_num, source_timing)
