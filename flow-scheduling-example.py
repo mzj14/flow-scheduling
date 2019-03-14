2cd6ac0f6c0fb5842c172adf9aa6469ae8191c2b
mzj14 <mazijun2017@outlook.com>
2019-03-14 13:48:57 -0500

    add minor change in example program

--- flow-scheduling-example.py
+++ flow-scheduling-example.py
@@ -7,24 +7,23 @@ import case_debugger
 
 
 if __name__ == '__main__':
-
+    '''
     nh_per_rack, nr_l, nr_s = 2, 2, 2
     n, m, port_num, router_choices = topology.leaf_spine_net(nh_per_rack, nr_l, nr_s)
     min_dest_num, max_dest_num, min_flow_num, max_flow_num, alpha, min_packet_num = 1, n - 1, 1, 3, 1.2, 10
     dests, flow_num, packet_num = flows.generate(n, min_dest_num, max_dest_num, min_flow_num, max_flow_num,
                                                  alpha, min_packet_num)
-
+    
     min_start_time, max_start_time, min_interval, max_interval = 0, 2, 1, 2
     source_timing = flows.timing(flow_num, packet_num, min_start_time, max_start_time, min_interval, max_interval)
+    '''
 
-    '''
-    flow_data_file = "backup/vialation-case-1.txt"
+    flow_data_file = "backup/violation-case-1.txt"
     n, m, nh_per_rack, nr_l, nr_s, dests, port_num, router_choices, flow_num, packet_num, source_timing = case_debugger.readFromFile(flow_data_file)
-    '''
 
     topology.display(n, m, nh_per_rack, nr_l, nr_s, port_num, router_choices)
     flows.display(n, dests, flow_num, packet_num, source_timing)
 
     scheduler.optimal_scheduling(n, m, dests, port_num, router_choices, flow_num, packet_num, source_timing)
-    # print(router_prefers, sender_prefers)
+
 
